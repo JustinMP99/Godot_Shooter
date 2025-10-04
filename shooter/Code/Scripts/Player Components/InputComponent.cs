362d3f3ff912cda50cbab0a7fc173ebaac38b2da
Justin Philie <115652134+JustinMP99@users.noreply.github.com>
2025-10-04 16:08:46 -0400

    Version 0.3.10
    > Began Player input rework

---
+++ shooter/Code/Scripts/Player Components/InputComponent.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public partial class InputComponent : Node
+{
+
+    [ExportCategory("Core Data")]
+    private Vector3 direction;
+    private Vector3 rotation;
+
+
+}
